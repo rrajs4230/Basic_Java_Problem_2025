e283d0620600b8a8fa23d2e31e85e0f451c861ef
Rahul <rrajs4230@gmail.com>
2025-01-05 17:03:56 +0530

    GenerateTable completed

--- src/com/basic/problem/GenerateTable.java
+++ src/com/basic/problem/GenerateTable.java
@@ -1,10 +1,33 @@
 package com.basic.problem;
 
+import java.util.Scanner;
+
 public class GenerateTable {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("Enter Table Number:");
+		
+	    int n =scan.nextInt();
+	    
+	    generateTable(n);
+		
 
+	}
+	
+	public static void  generateTable(int n){
+		
+		
+		for(int i=1; i<=10; i++) {
+			
+			System.out.println(n+"*"+i+"="+(n*i));
+			
+			
+		}
+		
+		
 	}
 
 }
